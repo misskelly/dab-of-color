a6b4b859348e3668f5cf4a24ec17e318dd701754
Kelly Zick <kllyzck@gmail.com>
2019-07-09 14:58:32 -0600

    Add grid layout to app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,8 +7,24 @@ const App = () => (
   
 
   <main className="app">
+    <header className="header">
     <h1>Palette Picker</h1>
+
+    </header>
     <Unicorn />
+    <section className="palettes-big">
+      PALETTES
+    </section>
+    <section className="new-palette-section">
+      COLOR GENERATOR
+    </section>
+    <section className="new-project-form">
+      <input type="text" placeholder="name"/>
+      <button type="submit" className="save-project-btn">Save</button>
+    </section>
+    <section className="gallery-section">
+      GALLERY
+    </section>
    
   </main>
 );
