576919b5ad12c1dc206b134295931cfbea713750
Kelly Zick <kllyzck@gmail.com>
2019-07-11 08:18:41 -0600

    Append little unicorn projects

--- src/components/ProjectGallery/index.js
+++ src/components/ProjectGallery/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import uniqid from 'uniqid';
 import Project from '../Project';
 
-const Gallery = () => {
-  // set up for redux
-  // map through projects, return a project component
 
-  return (
-    <div>
-      <Project />
-    </div>
-  )
+export class Gallery extends Component {
+  render() {
+    const { allProjects } = this.props;
+      const projects = (unicorns) => {
+        const key = uniqid();
+        const lilUnis = unicorns.map(uni => (
+          <Project
+            key={key}
+            name={uni.name}
+            palettes={uni.palettes}
+          />
+        ));
+        return lilUnis;
+      };
+      return (
+        <section className="project-gallery">
+          { projects(allProjects) }
+        </section>
+      );
+    
+  }
 }
 
-export default Gallery;
+export const mapStateToProps = state => ({
+  currentColors: state.currentColors,
+  allProjects: state.allProjects
+});
+
+
+export default connect(mapStateToProps)(Gallery);
