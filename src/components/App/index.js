ffdfa51afaca4acd2d400764917b2674ac757334
Kelly Zick <kllyzck@gmail.com>
2019-08-27 09:22:21 -0600

    Update linter and dependencies

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,31 +1,21 @@
 import React from 'react';
 import Unicorn from '../Picker';
 
-
-
 const App = () => (
-  
-
   <main className="app">
     <header className="header">
-    <h1>Palette Picker</h1>
-
+      <h1>Palette Picker</h1>
     </header>
     <Unicorn />
-    <section className="palettes-big">
-      PALETTES
-    </section>
-    <section className="new-palette-section">
-      COLOR GENERATOR
-    </section>
+    <section className="palettes-big">PALETTES</section>
+    <section className="new-palette-section">COLOR GENERATOR</section>
     <section className="new-project-form">
-      <input type="text" placeholder="name"/>
-      <button type="submit" className="save-project-btn">Save</button>
+      <input type="text" placeholder="name" />
+      <button type="submit" className="save-project-btn">
+        Save
+      </button>
     </section>
-    <section className="gallery-section">
-      GALLERY
-    </section>
-   
+    <section className="gallery-section">GALLERY</section>
   </main>
 );
 
