2eee9ad172685ecc4499268f056e27daf7f10e04
Kelly Zick <kllyzck@gmail.com>
2019-09-11 00:56:25 -0600

    Display current palettes

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -5,11 +5,34 @@ import Unicorn from '../../components/Unicorn';
 import Gallery from '../../components/ProjectGallery';
 import * as actions from '../../redux/actions';
 import TileContainer from '../TileContainer';
+import Palette from '../../components/Palette';
 import getProjects from '../../utils/thunks/getProjects';
 
 export class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentProject: '',
+      currentPalettes: [
+        {
+          name: 'coolman2',
+          color_1: '#8e5cd6',
+          color_2: '#adf49f',
+          color_3: '#efc8aa',
+          color_4: '#b4fca4',
+          color_5: '#e3e858'
+        },
+        {
+          name: 'Dazzle',
+          color_1: '#32213A',
+          color_2: '#383B53',
+          color_3: '#66717E',
+          color_4: '#D4D6B9',
+          color_5: '#D1CAA1'
+        }
+      ],
+      loading: false
+    };
   }
 
   componentDidMount() {
@@ -28,18 +51,30 @@ export class App extends Component {
   };
 
   render() {
-    // const { allProjects, loading } = this.state;
+    const { currentProject, currentPalettes, loading } = this.state;
+    const palettes = currentPalettes.map(pal => {
+      const { name, color_1, color_2, color_3, color_4, color_5 } = pal;
+      return (
+        <Palette
+          name={name}
+          colors={[color_1, color_2, color_3, color_4, color_5]}
+        />
+      );
+    });
     return (
       <main className="app">
         <header className="header">
           <h1>Palette Picker</h1>
         </header>
         <Unicorn />
-        <section className="palettes-big">PALETTES</section>
+        <section className="palettes-big">
+          <h3>Palettes</h3>
+          {loading === false && palettes}
+        </section>
         <section className="new-palette-section">
-          COLOR GENERATOR
+          <h3>Color Generator</h3>
           <TileContainer />
-          <h3 onClick={this.getRandomColors}>Generate Colors</h3>
+          <button onClick={this.getRandomColors}>Generate Colors</button>
         </section>
         <section className="new-project-form">
           <input type="text" placeholder="name" />
@@ -55,12 +90,13 @@ export class App extends Component {
   }
 }
 
-export const mapDispatchToProps = (dispatch) => ({
-  getCurrentColors: (colors) => dispatch(actions.getCurrentColors(colors)),
-  getProjects: (projectUrl, paletteUrl) => dispatch(getProjects(projectUrl, paletteUrl))
+export const mapDispatchToProps = dispatch => ({
+  getCurrentColors: colors => dispatch(actions.getCurrentColors(colors)),
+  getProjects: (projectUrl, paletteUrl) =>
+    dispatch(getProjects(projectUrl, paletteUrl))
 });
 
-export const mapStateToProps = (state) => ({
+export const mapStateToProps = state => ({
   currentColors: state.currentColors,
   allProjects: state.allProjects
 });
