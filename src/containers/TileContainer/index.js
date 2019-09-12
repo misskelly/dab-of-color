bb06a6d9a04afdbc82ccbe7f63b89b0ee9061f42
Kelly Zick <kllyzck@gmail.com>
2019-09-11 22:03:31 -0600

    Add lock color logic

--- src/containers/TileContainer/index.js
+++ src/containers/TileContainer/index.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import Tile from '../../components/Tiles';
 
 export class TileContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { locked: [false, false, false, false, false] };
+    this.lockColor = this.lockColor.bind(this);
+  }
+
+  lockColor = color => {
+    const { locked } = this.state;
+    const newLocked = locked.map((bool, index) => {
+      if (index === color) {
+        return !bool;
+      } else {
+        return bool;
+      }
+    });
+    this.setState({ locked: newLocked });
+  };
+
   render() {
-    const displayTiles = this.props.currentColors.map( color => <Tile key={color} color={color}/>)
-    return (
-      <div className='tile-container'>
-        {displayTiles}
-      </div>
-    )
+    const displayTiles = this.props.currentColors.map((color, index) => {
+      const isLocked = this.state.locked[index];
+      return (
+        <Tile
+          key={color}
+          color={color}
+          lockColor={this.lockColor}
+          isLocked={isLocked}
+        />
+      );
+    });
+    return <div className="tile-container">{displayTiles}</div>;
   }
 }
 
-export const mapStateToProps = (state) => ({
+export const mapStateToProps = state => ({
   currentColors: state.currentColors
-})
+});
 
-export default connect(mapStateToProps)(TileContainer)
+export default connect(mapStateToProps)(TileContainer);
