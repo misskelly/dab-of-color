9b31b79824b432a75b37019dee17d96106d4eccf
Kelly Zick <kllyzck@gmail.com>
2019-10-16 17:38:40 -0600

    Refactor form, add conditional rendering

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,9 @@
-/* eslint-disable no-alert */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import randomColor from 'randomcolor';
 import * as actions from '../../redux/actions';
 import currentProject from '../../utils/thunks/currentProject';
+import postNew from '../../utils/apiCalls/postNew';
 import { cleanPalette } from '../../utils/cleaners/cleanPalette';
 
 export class UniForm extends Component {
@@ -12,48 +12,35 @@ export class UniForm extends Component {
     this.state = {
       unicornName: '',
       paletteName: '',
-      featuredPalettes: [],
       editView: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount = () => {
-    const { featuredProject } = this.props;
-    if (featuredProject !== undefined) {
-      console.log('featured project: ', featuredProject)
-    }
-  }
-  
+
   handleChange = (e, field) => {
     this.setState({ [field]: e.target.value });
   };
-  
+
+  savePalette = async palette => {
+    return await postNew('palettes', palette);
+  };
+
   handleSaveName = e => {
-    e.preventDefault()
-    const { 
-      // featuredProject, 
-      currentProject } = this.props;
+    e.preventDefault();
+    const { currentProject } = this.props;
     const { unicornName } = this.state;
     currentProject({ name: unicornName, palettes: [] });
-    this.setState({editView: true})
-
-  }
+    this.setState({ editView: true });
+  };
 
   handleSubmit = e => {
     e.preventDefault();
-    // const { unicornName } = this.state;
-    // const { currentProject, currentColors } = this.props;
-    this.addPalette();
-    // console.log('currentColors:  ', currentColors)
-    // currentProject({ name: unicornName, palettes: });
-    // this.setState({
-    //   unicornName: '',
-    //   paletteName: '',
-    //   featuredPalettes: [],
-    //   editView: false
-    // });
+    const { featuredProject } = this.props;
+    if (featuredProject.id) {
+      this.addPalette();
+    }
   };
 
   generateNewPalette = () => {
@@ -66,73 +53,92 @@ export class UniForm extends Component {
   };
 
   addPalette = () => {
-    const { currentColors } = this.props;
-    const { paletteName, featuredPalettes } = this.state;
-    const newPalette = cleanPalette(paletteName, currentColors);
-    const palettes = featuredPalettes.concat(newPalette);
-    console.log('palettes:  ', palettes)
+    const { currentColors, featuredProject } = this.props;
+    const { paletteName } = this.state;
+    const paletteToAdd = cleanPalette(
+      paletteName,
+      currentColors,
+      featuredProject.id
+    );
+    this.savePalette(paletteToAdd);
+    this.updateFeatured(paletteToAdd);
+    this.resetPaletteForm();
   };
 
-  resetForm = () => {
+  updateFeatured = async newPalette => {
+    const { featuredProject, currentProject } = this.props;
+    const { id, name } = featuredProject;
+    const palettes = featuredProject.palettes.concat(newPalette);
+    const updatedProject = { id, name, palettes };
+    const update = await currentProject(updatedProject);
+    return update;
+
+  };
+
+  resetPaletteForm = () => {
     this.generateNewPalette();
     return this.setState({
-      unicornName: '',
       paletteName: ''
     });
-  }
+  };
+
+  formReset = () => {
+    const { setCurrentProject } = this.props;
+    this.setState({ unicornName: '', editView: false });
+    setCurrentProject({});
+  };
 
   render() {
     const { unicornName, paletteName, editView } = this.state;
     const { featuredProject } = this.props;
-    if(featuredProject !== undefined) {
-      console.log('featured project: ',featuredProject)
-    }
     return (
       <section className="form-section">
-        {!editView &&
-        <form className="new-uni-form" onSubmit={this.handleSaveName}>
-          <fieldset className="name-form">
-            <label className="name-input-label" htmlFor="name-input">
-              Name this unicorn:
-              <input
-                type="text"
-                value={unicornName}
-                className="name-input"
-                placeholder="Bob"
-                onChange={e => this.handleChange(e, 'unicornName')}
+        {!editView && (
+          <form className="new-uni-form" onSubmit={this.handleSaveName}>
+            <fieldset className="name-form">
+              <label className="name-input-label" htmlFor="name-input">
+                Name this unicorn:
+                <input
+                  type="text"
+                  value={unicornName}
+                  className="name-input"
+                  placeholder="Bob"
+                  onChange={e => this.handleChange(e, 'unicornName')}
                 />
-            </label>
-            <button className="name-uni-btn" type="submit">Save</button>
-          </fieldset>
-        </form>
-        }
-        {editView &&
+              </label>
+              <button className="name-uni-btn" type="submit">
+                Save
+              </button>
+            </fieldset>
+          </form>
+        )}
+        {editView && (
           <form className="save-project-form" onSubmit={this.handleSubmit}>
             <label
               className="palette-name-input-label"
               htmlFor="palette-name-input"
-              >
+            >
               Name this color scheme:
               <input
-              type="text"
-              value={paletteName}
-              className="palette-name-input"
-              placeholder="Cotton Candy"
-              onChange={e => this.handleChange(e, 'paletteName')}
+                type="text"
+                value={paletteName}
+                className="palette-name-input"
+                placeholder="Cotton Candy"
+                onChange={e => this.handleChange(e, 'paletteName')}
               />
             </label>
-            <button
-              type="button"
-              className="add-palette-btn"
-              onClick={this.addPalette}
-              >
-              +
-            </button>
-            <button className="name-btn" type="submit">
+            <button type="submit" className="add-palette-btn">
               Save
             </button>
+            <button
+              className="new-uni-btn"
+              type="button"
+              onClick={this.formReset}
+            >
+              Create New Unicorn
+            </button>
           </form>
-        }
+        )}
       </section>
     );
   }
